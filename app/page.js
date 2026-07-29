21d7febad6444ad755088b094960c39f06af0908
f1rstlabel <saputraregykurnia@gmail.com>
2026-07-29 02:46:17 +0000

    Percobaan Awal Rubah Halaman

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>🚀 Monitoring2026 berjalan!</h1>
+      <h1>Tes Halaman Awal Monitoring2026 berjalan!</h1>
       <p>Deploy via GitHub Actions CI/CD + ArgoCD ke k3s.</p>
     </main>
   );
